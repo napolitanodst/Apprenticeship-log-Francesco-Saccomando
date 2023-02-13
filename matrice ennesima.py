aee13849f416cb6f6d21a8112ef544fce4dd3d4e
Francesco Saccomando Ciaramella <124696328+BioFrsk@users.noreply.github.com>
2023-02-13 16:23:22 +0100

    Update matrice ennesima.py

--- matrice ennesima.py
+++ matrice ennesima.py
@@ -4,7 +4,7 @@ y = int(input("Inserisci numero colonne"))
 for i in range (x):
      num = []
      for j in range (y) :
-          n = int(input("Inserisci un numero"))
+          n = float(input("Inserisci un numero"))
           num.insert (i,n)
      matrice.append(num)
 
