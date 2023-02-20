c82d249dcfb42c541fd7a2543c45b4c57e7dc737
Francesco Saccomando Ciaramella <124696328+BioFrsk@users.noreply.github.com>
2023-02-20 11:13:33 +0100

    Delete other_if7.py

--- other_if7.py
+++
@@ -1,6 +0,0 @@
-a,b= int(input("Inserisci un numero:")),int(input("Inserisci un altro numero:"))
-if a==0 or b==0:
-    print("Il risultato della moltiplicazione è 0")
-else:
-    r= a*b
-    print ("Il risultato della moltiplicazione è:",r)
